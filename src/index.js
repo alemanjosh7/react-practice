22ceec5b6278d37231ab28766a2afd6cf3b1248a
alemanjosh7 <40409070+alemanjosh7@users.noreply.github.com>
2020-04-10 19:51:19 -0600

    Deleting unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+// const React = require('react')
 import React from 'react';
+// Babel nos permite usar versiones recientes de JS
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
